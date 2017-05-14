e1bab843660d5b7319d008fde2a740df9a9696bb
kredes <andresinsaurralde@gmail.com>
2017-05-14 18:35:33 +0200

    2.2 + 2.1 fixes

--- program.sh
+++ program.sh
@@ -1,18 +1,20 @@
 import System.IO
 import System.Random
 
+
+-- 2.1
 type Ident = String
 
 data Command a =
     Seq [Command a]
   | Copy Ident Ident
-  | TAssign Ident Tube
+  | TAssign Ident TExpr
   | CAssign Ident Connector
   | DeclareVector Ident NExpr
   | Split Ident Ident Ident
   | Input Ident
   | Print NExpr
-  | Draw Ident
+  | Draw TExpr
   | Pop Ident Ident
   | Push Ident Tube
   | Cond (BExpr a) (Command a) (Command a)
@@ -49,4 +51,38 @@ data TExpr a =
 data CExpr a =
     CVar Ident
   | Connector (NExpr a)
+  
+
+-- 2.2 - Shows a command with the given indentation (s)
+showind :: Show a => Command a -> String -> String
+showind (Seq []) s    = ""
+showind (Seq (x:xs)) s  =
+  (showind x s) ++ "\n" ++ (showind (Seq xs) s)
+
+showind (Cond be c1 (Seq [])) s =
+  s ++ "IF " ++ (show be) ++ " THEN\n" ++
+  (showind c1 ("  " ++ s)) ++ s ++ "END\n"
+
+showind (Cond be c1 c2) s =
+  s ++ "IF " ++ (show be) ++ " THEN\n" ++
+  (showind c1 ("  " ++ s)) ++ s ++ "ELSE\n" ++
+  (showind c2 ("  " ++ s)) ++ s ++ "END"
+
+showind (TAssign x y) s = s ++ x ++ " = " ++ (show y)
+showind (CAssign x y) s = s ++ x ++ " = " ++ (show y)
+showind (DeclareVector x y) s = s ++ x ++ " = TUBEVECTOR OF " ++ (show y)
+showind (Split x y z) s = s ++ "(" ++ x ++ ", " ++ y ++ ") = SPLIT " ++ z
+
+showind (Input x) s = s ++ "INPUT " ++ x
+showind (Print x) s = s ++ "PRINT " ++ (show x)
+showind (Draw x) s = s ++ (show x)
+
+showind (Pop x y) s = s ++ "POP " ++ x ++ " " ++ y
+showind (Push x y) s = s ++ "PUSH " ++ x ++ " " ++ y
+showind (Loop be c1) s =
+  s ++ "WHILE " ++ (show be) ++ "\n" ++ s ++ "DO\n" ++
+  (showind c1 ("  " ++ s)) ++ s ++ "END"
     
+instance Show a => Show(Command a) where
+  show x = showind x ""
+  
